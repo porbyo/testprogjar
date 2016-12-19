62c423e1d736a2af55d0aa31789cc91fcd79990b
Rochman Soleh <rochmansoleh@hotmail.com>
2016-12-19 23:45:14 +0700

    Update server.py

--- server.py
+++ server.py
@@ -4,6 +4,24 @@ from time import sleep
 class ClientChannel(PodSixNet.Channel.Channel):
     def Network(self, data):
         print data
+
+    def Network_place(self, data):
+        #deconsolidate all of the data from the dictionary
+     
+        #x of placed line
+        x = data["x"]
+     
+        #y of placed line
+        y = data["y"]
+     
+        #player number (1 or 0)
+        num=data["num"]
+     
+        #id of game given by server at start of game
+        self.gameid = data["gameid"]
+     
+        #tells server to place line
+        self._server.placeLine(x, y, data, self.gameid, num)
  
 class BoxesServer(PodSixNet.Server.Server):
  
@@ -28,6 +46,10 @@ class BoxesServer(PodSixNet.Server.Server):
         self.games = []
         self.queue = None
         self.currentIndex=0
+    def placeLine(self, x, y, data, gameid, num):
+        game = [a for a in self.games if a.gameid==gameid]
+        if len(game)==1:
+            game[0].placeLine(x, y, data, num)
 
 class Game:
     def __init__(self, player0, currentIndex):
@@ -40,6 +62,19 @@ class Game:
         self.player1=None
         #gameid of game
         self.gameid=currentIndex
+
+    def placeLine(self, x, y, data, num):
+        #make sure it's their turn
+        if num==self.turn:
+            self.turn = 0 if self.turn else 1
+            #place line in game
+            if num:
+                self.boardo[y][x] = True
+            else:
+                self.boardx[y][x] = True
+            #send data and turn data to each player
+            self.player0.Send(data)
+            self.player1.Send(data)
 
 print "STARTING SERVER ON LOCALHOST"
 boxesServe=BoxesServer()
