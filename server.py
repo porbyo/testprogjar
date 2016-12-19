38bf06306bf2176ddad89865b9a151f7b86fa16e
Rochman Soleh <rochmansoleh@hotmail.com>
2016-12-19 21:46:19 +0700

    Update server.py

--- server.py
+++ server.py
@@ -1,27 +1,11 @@
 import PodSixNet.Channel
 import PodSixNet.Server
 from time import sleep
-
 class ClientChannel(PodSixNet.Channel.Channel):
     def Network(self, data):
         print data
  
 class BoxesServer(PodSixNet.Server.Server):
-    def __init__(self, *args, **kwargs):
-        PodSixNet.Server.Server.__init__(self, *args, **kwargs)
-        self.games = []
-        self.queue = None
-        self.currentIndex=0
-    
-class Game:
-    def __init__(self, player0, currentIndex):
-        self.turn = 0
-        self.owner=[[False for x in range(3)] for y in range(3)]
-        self.boardx = [[False for x in range(3)] for y in range(3)]
-        self.boardy = [[False for x in range(3)] for y in range(3)]
-        self.player0=player0
-        self.player1=None
-        self.gameid=currentIndex
  
     channelClass = ClientChannel
  
@@ -31,15 +15,32 @@ class Game:
             self.currentIndex+=1
             channel.gameid=self.currentIndex
             self.queue=Game(channel, self.currentIndex)
-            
         else:
             channel.gameid=self.currentIndex
             self.queue.player1=channel
-            self.queue.player0.Send({"action": "startgame","playernum":0,"player":0, "gameid": self.queue.gameid})
-            self.queue.player1.Send({"action": "startgame","playernum":1,"player":1, "gameid": self.queue.gameid})
+            self.queue.player0.Send({"action": "startgame","player":0, "gameid": self.queue.gameid})
+            self.queue.player1.Send({"action": "startgame","player":1, "gameid": self.queue.gameid})
             self.games.append(self.queue)
             self.queue=None
- 
+        
+    def __init__(self, *args, **kwargs):
+        PodSixNet.Server.Server.__init__(self, *args, **kwargs)
+        self.games = []
+        self.queue = None
+        self.currentIndex=0
+
+class Game:
+    def __init__(self, player0, currentIndex):
+        # whose turn (1 or 0)
+        self.turn = 0
+        self.boardo = [[False for x in range(3)] for y in range(3)]
+        self.boardx = [[False for x in range(3)] for y in range(3)]
+        #initialize the players including the one who started the game
+        self.player0=player0
+        self.player1=None
+        #gameid of game
+        self.gameid=currentIndex
+
 print "STARTING SERVER ON LOCALHOST"
 boxesServe=BoxesServer()
 while True:
